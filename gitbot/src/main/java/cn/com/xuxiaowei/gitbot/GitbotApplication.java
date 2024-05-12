7a0b4aaaa92e62765dd27e229101e3765ae1e8d8
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2024-05-13 07:54:26 +0800

    :tada: init gitbot project

---
+++ gitbot/src/main/java/cn/com/xuxiaowei/gitbot/GitbotApplication.java
@@ -0,0 +1,13 @@
+package cn.com.xuxiaowei.gitbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitbotApplication.class, args);
+	}
+
+}
---
+++ gitbot/src/test/java/cn/com/xuxiaowei/gitbot/GitbotApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.com.xuxiaowei.gitbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
