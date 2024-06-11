963993775665b205c382907a50c2fc9928bdceb3
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2024-06-11 19:06:00 +0800

    :coffin: clean

--- gitbot-ui/src/views/HomeView.vue
+++ gitbot-ui/src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="home">
+    <h1>HomeView</h1>
   </main>
 </template>
